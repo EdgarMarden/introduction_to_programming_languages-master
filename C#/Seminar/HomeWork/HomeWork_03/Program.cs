654873e17790834956218ed982da5e0094793197
Edgar.Mardanov <edgar.mardanov@yadenx.ru>
2022-08-26 14:06:51 +0300

    Complete_HomeWork_03

--- C#/Seminar/HomeWork/HomeWork_03/Program.cs
+++ C#/Seminar/HomeWork/HomeWork_03/Program.cs
@@ -5,7 +5,7 @@
 //12821 -> да
 //23432 -> да
 
-//Решение задачи №19
+/* //Решение задачи №19
 
 bool FindPoly(int anyNumber)
 {
@@ -23,7 +23,7 @@ bool FindPoly(int anyNumber)
             c = a == b;
             b = anyNumber % 10;
         }
-        else break;
+        else;
     }
     return c;
 }
@@ -33,45 +33,6 @@ int userNumber = Convert.ToInt32(Console.ReadLine());
 bool Boolean = FindPoly(userNumber);
 
 Console.WriteLine($"{userNumber} -> {Boolean}");
-
-/*
-    while(a == b)
-    {
-        if(a == b)
-        {
-            a *= 10;
-            b = anyNumber / 10 % 10;
-            Console.WriteLine($"{anyNumber} -> Yes");
-        }
-        else Console.WriteLine($"{anyNumber} -> No");
-    }
-}
-*/
-
-
-//Решение задачи №19 - 2 НЕПРАВИЛЬНОЕ
-/*
-void FindPoly(int anyNumber)
-{
-    int b = anyNumber % 10;
-    int a = anyNumber /;
-    while (a > 99)
-    {   
-        a = a / 10;
-        if(a == b)
-            while (a == b)
-            {
-                if (a > 999) a = a / 100;
-                else a = a / 100;
-                Console.WriteLine($"{anyNumber} -> Yes");
-            }
-        else Console.WriteLine($"{anyNumber} -> No");
-    }
-}
-
-Console.Write("Hi people, enter please any number from 10 to 999 999: ");
-int userNumber = Convert.ToInt32(Console.ReadLine());
-FindPoly(userNumber);
 */
 
 //Задача 21
@@ -80,10 +41,48 @@ FindPoly(userNumber);
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
+/*//Решение задачи №21
 
+double FindSqrt(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1)); // вычисляет квадратный корень √ ( x B − x A ) 2 + ( y B − y A ) 2 + ( z B − z A ) 2
+}
+
+Console.Write("Enter the 1st coordinate, axis X: ");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the 1st coordinate, axis Y: ");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the 1st coordinate, axis Z: ");
+double zA = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter the 2st coordinate, axis X: ");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the 2st coordinate, axis Y: ");
+double yB = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the 2st coordinate, axis Z: ");
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double distance = FindSqrt(xA, yA, zA, xB, yB, zB);
+Console.WriteLine($"A ({xA}, {yA}, {zA}); B ({xB}, {yB}, {zB}) -> {distance}");
+*/
 
 //Задача 23
 //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
+
+void Qudrat(int completeNumber)
+{
+    int n = 1;
+    while (n <= completeNumber)
+    {
+        Console.Write($"{n * n * n} ");
+        n++;
+    }
+}
+
+Console.Write("Input the any number: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+Qudrat(userNumber);
