56f1c0b9b73cce30412886d5ee4e43bdd39d4285
edgar.mardanov <edgar.mardanov@yandex.ru>
2022-10-04 14:21:43 +0300

    seminar_09_processed

--- C#/Seminar/Seminar_09/Program.cs
+++ C#/Seminar/Seminar_09/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿// Задайте значение N.Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+
+Console.Write("Input number of M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number of N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void ShowNums (int n)
+{
+    if(n > 1)
+    {
+        Console.Write(n +" ");
+        if(n > 1) ShowNums(n - 1);
+    }
+
+    Console.Write(n +" ");
+}
+
+// ShowNums(numberN);
+
+// Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр.
+
+int SumOfDigigts (int n)
+{
+    if(n > 0) return SumOfDigigts(n / 10) + n % 10;
+    else return 0;
+}
+
+// Console.Write(SumOfDigigts(numberN)); 
+
+// Задайте значения M и N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от M до N.
+
+void ShowIntervalNums (int m, int n)
+{
+    if(m > n) ShowIntervalNums(n - 1);
+
+    Console.Write(m +" ");
+}
+
+ShowIntervalNums(numberM, numberN);
+
+// Напишите программу, которая на вход принимает два числа A и B,
+// и возводит число А в целую степень B с помощью рекурсии.
